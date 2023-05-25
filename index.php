205432b790969fc7c63709d42d7a38a66badbe07
lacoca <032287d@utea.edu.pe>
2023-05-24 21:29:33 -0500

    trabajos solid

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+include 'CoffeeShop.php';
+include 'Menu.php';
+
+$products = [
+    ['id' => 1, 'name' => 'Negro', 'price' => '25'],
+    ['id' => 2, 'name' => 'Latté', 'price' => '35'],
+    ['id' => 3, 'name' => 'Capuchino', 'price' => '45'],
+    ['id' => 4, 'name' => 'Expreso', 'price' => '55'],
+];
+
+$shop = new Coffeeshop;
+
+$shop->addProducts($products);
+
+$menu = new Menu;
+
+echo $menu->viewMenu($shop->getProducts());
+
+?>
